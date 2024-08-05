54a443f7afe11d6410a2d892e7aa300f145b0ac4
AtsDenli <satadenli@gmail.com>
2024-08-05 11:21:39 +0100

    Fetches weather correctly

--- scripts.js
+++ scripts.js
@@ -8,14 +8,12 @@ searchBtn.addEventListener('click', async () => {
     const location = locInput.value;
     console.log("button clicked");
     if (location){
-        coords = await getCoords(location);
+        var coords = await getCoords(location);
         console.log("Coords received");
-        console.log(coords); 
-        longitude = coords[0][2][1][1];
-        latitude = coords[0][2][1][0]; 
-        weatherdata = await getWeather(longitude,latitude);
+        const coordinates = coords.geometry.location;
+        const weatherdata = await getWeather(coordinates.lng, coordinates.lat);
         console.log(weatherdata);
-        console.log("Achieved");
+        console.log("Success");
     }
 });
 
@@ -27,8 +25,8 @@ async function getCoords(location) {
         throw new Error('Network response was not ok.');
     }
     const data = await response.json();
-    return data;
-}
+    return data.results[0];
+} 
 
 async function getWeather(longitude, latitude) {
     const params = {
@@ -40,7 +38,7 @@ async function getWeather(longitude, latitude) {
     };
 
     console.log("getting weather")
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=precipitation_probability,precipitation,snow_depth,pressure_msl,cloud_cover,wind_speed_180m,wind_direction_120m,temperature_120m&daily=weather_code,sunrise,sunset,uv_index_max,precipitation_hours&forecast_days=1`, {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=precipitation_probability,precipitation,snow_depth,pressure_msl,cloud_cover,wind_speed_180m,wind_direction_120m,temperature_120m&daily=weather_code,sunrise,sunset,uv_index_max,precipitation_hours&forecast_days=1`, {
         method: 'GET',
         params: params
       });
